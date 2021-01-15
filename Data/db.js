8a352be5a3548efc01cfc00da57123a3726c8be3
DiegoKraenau <diegokraenau@gmail.com>
2021-01-15 02:15:32 -0500

    Sequelize

---
+++ Data/db.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+//Clases
+const FilmModel = require('../Models/Films');
+
+
+//Connection 
+const sequelize = new Sequelize('basedatos', 'root', 'admin', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3306'
+});
+
+//Map classes
+const Film = FilmModel(sequelize, Sequelize)
+
+//After mapping
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('Created tables')
+    })
+
+
+
+
+module.exports = {
+    Film
+}
+
+
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+const apiRouter = require('./routes/api');
+
+const app = express();
+
+
+require('./Data/db');
+
+//Middlewares
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({extended:true}));
+app.use('/api',apiRouter);
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Se pudo levantar el servidor")
+});
+
+
+
