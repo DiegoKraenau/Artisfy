92eae59b6225ad9a1685e63168f4d1950b026036
DiegoKraenau <diegokraenau@gmail.com>
2021-01-16 21:37:14 -0500

    Actualizando

--- backend-practicando/index.js
+++ backend-practicando/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParse = require('body-parser');
 const apiRouter = require('./routes/api');
+var cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 require('./Data/db');
 
 //Middlewares
+app.use(cors());
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({extended:true}));
 app.use('/api',apiRouter);
@@ -15,7 +17,7 @@ app.use('/api',apiRouter);
 
 
 
-app.listen(3000,()=>{
+app.listen(3090,()=>{
     console.log("Se pudo levantar el servidor")
 });
 
